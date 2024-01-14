0abebcd3181f82c912f6158121dbbc7d0654db7a
mico845 <1120845871@qq.com>
2024-01-14 23:53:04 +0800

    新增功能：利用子CMake添加ARM_DSP库

--- UserApp/Main.cpp
+++ UserApp/Main.cpp
@@ -3,6 +3,7 @@
 //
 #include "common_inc.h"
 #include "OnChip/FLASH/FLASH.hpp"
+#include "arm_math.h"
 
 using namespace Platform::HardWare;
 
@@ -10,30 +11,24 @@ using namespace Platform::HardWare;
 static void Task_Key(void);
 static void Task_Led_Flash(void);
 static void Task_ADC_Finished(void);
-static void Task_UART_Finished(void);
 
 //需要执行的任务
 static void DDS_WaveStart(void);
-static void Flash_Test(void);
 
  void Init(void)
 {
     led.PinkishRed();
     cout << "Hello World" << '\n';
-    Flash_Test();
-    lcd << "hello" << TFT_LCD::endl;
+    float32_t a = arm_sin_f32( 3/2.0 * 3.1415);
+    lcd << a << TFT_LCD::endl;
     timer.RegisterCallback(KeyScan10ms).Start(100);
-    // adc.Start(1000);
-    // cout.Receive_Start_DMA();
- //    DDS_WaveStart();
 }
 
 void Loop(void)
 {
     Task_Key();
     Task_Led_Flash();
-    Task_ADC_Finished();
-    Task_UART_Finished();
+    // Task_ADC_Finished();
 }
 
 static void Task_Key(void)
@@ -81,51 +76,10 @@ static void Task_ADC_Finished(void)
     }
 }
 
-static void Task_UART_Finished(void)
-{
-    static uint8_t buffer[256] = {0};
-    if (cout.Is_ReceivedFinished())
-    {
-        cout.GetReceivedData((uint8_t *)buffer, cout.Get_ReceivedSize());
-        cout << (char *)buffer << '\n';
-    }
-}
-
 static void DDS_WaveStart(void)
 {
     #define DDS_ROM_WAVE_SIZE (4096 * 16)
     static uint16_t SinWave_ROM[DDS_ROM_WAVE_SIZE];
     generateSineWaveTable(SinWave_ROM, DDS_ROM_WAVE_SIZE, 3);
     DDS_Config(dds, 4096000,SinWave_ROM, DDS_ROM_WAVE_SIZE, 40000, 0).Start();
-}
-
-static void Flash_Test(void)
-{
-#if 0   //读写变量
-    uint8_t a;
-    uint16_t b;
-    uint32_t c;
-
-    //擦除扇区
-    Platform_Flash_EraseCpuFlash((uint32_t)CPUFLASH_ADDRESS_32B(0));
-    a = 15;
-    b = 18;
-    c = 19;
-    Platform_FLash_WriteCpuFlash((uint32_t)CPUFLASH_ADDRESS_32B(0), (uint8_t *)&a, sizeof (a));
-    Platform_FLash_WriteCpuFlash((uint32_t)CPUFLASH_ADDRESS_32B(1), (uint8_t *)&b, sizeof (b));
-    Platform_FLash_WriteCpuFlash((uint32_t)CPUFLASH_ADDRESS_32B(2), (uint8_t *)&c, sizeof (c));
-    cout << (uint8_t)*(CPUFLASH_ADDRESS_32B(0))  << '\n';
-    cout << (uint16_t)*(CPUFLASH_ADDRESS_32B(1)) << '\n';
-    cout << (uint32_t)*(CPUFLASH_ADDRESS_32B(2)) << '\n';
-#else        //读写结构体
-    struct D{
-     int a = 9;
-     int b = 16;
-    }d;
-    D* pd;
-    Platform_Flash_EraseCpuFlash(ADDR_FLASH_SECTOR_3_BANK2);
-    Platform_FLash_WriteCpuFlash(ADDR_FLASH_SECTOR_3_BANK2, (uint8_t *)&d, sizeof (d));
-    pd = (D *)ADDR_FLASH_SECTOR_3_BANK2;
-    cout << "d.a = " << pd->a << " d.b = " << pd->b << '\n';
-#endif
 }
